00655a5363f1177d911ba6d9358b10ae4c6c6eb9
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-15 11:34:46 +0200

    initial commit

---
+++ dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/DormantBitcoinSeekerApplication.java
@@ -0,0 +1,13 @@
+package com.dellarosamarco.dormantbitcoinseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormantBitcoinSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormantBitcoinSeekerApplication.class, args);
+	}
+
+}
---
+++ dormant-bitcoin-seeker/src/test/java/com/dellarosamarco/dormantbitcoinseeker/DormantBitcoinSeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dellarosamarco.dormantbitcoinseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormantBitcoinSeekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
