99012d1d2c2a946387b73f2c3829532579219e6b
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-16 11:03:25 +0200

    update bitcoin service

--- dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/service/BitcoinService.java
+++ dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/service/BitcoinService.java
@@ -3,16 +3,17 @@ package com.dellarosamarco.dormantbitcoinseeker.service;
 import com.dellarosamarco.dormantbitcoinseeker.utils.BitcoinUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.HexFormat;
+import java.math.BigInteger;
+import java.security.interfaces.ECKey;
 
 @Service
 public class BitcoinService {
     public String randomPrivateKey(){
         byte[] bytes = BitcoinUtils.randomBytes(32);
-        return bytesToString(bytes);
+        return BitcoinUtils.bytesToHex(bytes);
     }
 
-    public String bytesToString(byte[] bytes){
-        return HexFormat.of().formatHex(bytes);
+    public String randomAddress(){
+        return "asd";
     }
 }
--- dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/utils/BitcoinUtils.java
+++ dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/utils/BitcoinUtils.java
@@ -1,10 +1,12 @@
 package com.dellarosamarco.dormantbitcoinseeker.utils;
 
-import java.util.HexFormat;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class BitcoinUtils
 {
-    public static byte[] randomBytes(int length){
+    public static byte[] randomBytes(final int length){
         byte[] bytes = new byte[length];
 
         for(int n=0;n<length;n++) {
@@ -12,5 +14,41 @@ public class BitcoinUtils
         }
 
         return bytes;
+    }
+
+    public static String bytesToHex(final byte[] bytes){
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+
+        for (byte _byte : bytes) {
+            String hex = Integer.toHexString(0xff & _byte);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+
+    public static byte[] sha256(String data) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(data.getBytes(StandardCharsets.UTF_8));
+            return messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static byte[] sha256(byte[] data) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(data);
+            return messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
