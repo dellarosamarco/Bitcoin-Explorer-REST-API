a88eee3c12719e63d3b1dbaf1598a647dfd7d97f
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-17 20:52:20 +0200

    Added RipeMD160

--- dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/DormantBitcoinSeekerApplication.java
+++ dormant-bitcoin-seeker/src/main/java/com/dellarosamarco/dormantbitcoinseeker/DormantBitcoinSeekerApplication.java
@@ -1,8 +1,11 @@
 package com.dellarosamarco.dormantbitcoinseeker;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.security.Security;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +13,7 @@ public class DormantBitcoinSeekerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DormantBitcoinSeekerApplication.class, args);
+		Security.addProvider(new BouncyCastleProvider());
 	}
 
 }
